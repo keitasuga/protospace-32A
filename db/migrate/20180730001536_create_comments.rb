2c781e1ddcf1cba5d3e7159678384b3d582f658d
riho.fujino <riho.fujino@gmail.com>
2018-07-30 09:36:02 +0900

    reference型の書き方に修正

--- db/migrate/20180730001536_create_comments.rb
+++ db/migrate/20180730001536_create_comments.rb
@@ -1,9 +1,9 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-      t.integer  :user_id
-      t.integer  :prototype_id
-      t.text     :content
+      t.references  :user, null: false, foreign_key: true
+      t.references  :prototype, null: false, foreign_key: true
+      t.text        :content, null: false
       t.timestamps
     end
   end

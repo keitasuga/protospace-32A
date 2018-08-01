13ec8e483d871a2d17f2244adbe2424c4e08d04f
riho.fujino <riho.fujino@gmail.com>
2018-08-01 10:55:43 +0900

    スペルミス修正

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,7 +2,7 @@ class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many :captured_images, dependent: :destroy
   has_many :comments
-  has_many :tag_maps, dependant: :destroy
+  has_many :tag_maps, dependent: :destroy
   has_many :tags, through: :tag_maps
 
   accepts_nested_attributes_for :captured_images, reject_if: :reject_sub_images
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,4 @@
 class Tag < ActiveRecord::Base
-  has_many :tag_maps, dependant: :destroy
+  has_many :tag_maps, dependent: :destroy
   has_many :prototypes, through: :tag_maps
 end

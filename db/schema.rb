b0f122e447355285da1c6ca4899e68372fafe057
小田幸治 <swimmer0206@icloud.com>
2018-08-02 09:29:21 +0900

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180730001536) do
+ActiveRecord::Schema.define(version: 20180801012553) do
 
   create_table "captured_images", force: :cascade do |t|
     t.string  "content",      limit: 255
@@ -54,6 +54,22 @@ ActiveRecord::Schema.define(version: 20180730001536) do
 
   add_index "prototypes", ["user_id"], name: "index_prototypes_on_user_id", using: :btree
 
+  create_table "tag_maps", force: :cascade do |t|
+    t.integer  "tag_id",       limit: 4, null: false
+    t.integer  "prototype_id", limit: 4, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "tag_maps", ["prototype_id"], name: "fk_rails_41a8c86d66", using: :btree
+  add_index "tag_maps", ["tag_id"], name: "fk_rails_5f66989e5f", using: :btree
+
+  create_table "tags", force: :cascade do |t|
+    t.text     "name",       limit: 65535, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255,   default: "", null: false
     t.string   "encrypted_password",     limit: 255,   default: "", null: false
@@ -83,4 +99,6 @@ ActiveRecord::Schema.define(version: 20180730001536) do
   add_foreign_key "likes", "prototypes"
   add_foreign_key "likes", "users"
   add_foreign_key "prototypes", "users"
+  add_foreign_key "tag_maps", "prototypes"
+  add_foreign_key "tag_maps", "tags"
 end
